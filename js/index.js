f531354eef9fa4a50f49b0f1f1ca9c114fb57c55
Anastasia <18anastasiasneshko@gmail.com>
2021-07-24 13:25:06 +0300

    Add arrayTaks

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// 1. Даны числа 4, -2, 5, 19, -130, 0, 10. Найдите минимальное и максимальное число
+// // 1. Даны числа 4, -2, 5, 19, -130, 0, 10. Найдите минимальное и максимальное число
 const arrayOfNum = [4, -2, 5, 19, -130, 0, 10];
 
 function getMinNum (arrayOfNum) {
@@ -12,6 +12,7 @@ function getMinNum (arrayOfNum) {
   return minNum;
 }
 
+//Максимальное число
 function getMaxNum (arrayOfNum) {
   let maxNum = arrayOfNum[0];
 
@@ -24,7 +25,7 @@ function getMaxNum (arrayOfNum) {
 }
 
 
-// 2. Дан массив arr = [12, 15, 20, 25, 59, 79.] Найдите среднее арифметическое его элементов
+// // 2. Дан массив arr = [12, 15, 20, 25, 59, 79.] Найдите среднее арифметическое его элементов
 const arrayOfNum = [12, 15, 20, 25, 59, 79];
 
 function averageOfNum (count) {
@@ -38,13 +39,13 @@ function averageOfNum (count) {
 }
 
 
-// 3. Выведите на экран случайное целое число от 1 до 100 с помощью функции.
+// // 3. Выведите на экран случайное целое число от 1 до 100 с помощью функции.
 function getRandomNumber(){
   return Math.round(Math.random() * 100) + 1;
 }
 
 
-// 4. Заполните массив 10-ю случайными целыми числами.
+// // 4. Заполните массив 10-ю случайными целыми числами.
 let arrayOfNum = [];
 for (let i = 0; i < 10; i++) {
   arrayOfNum[i] = Math.floor(Math.random() * 10);
